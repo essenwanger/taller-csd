922b64f1237b2bc67cb8f335f918da22898cd5ef
Diego Clemente <dieg209@gmail.com>
2016-12-09 16:17:42 +0000

    mock ok

--- spec/faquiu_spec.rb
+++ spec/faquiu_spec.rb
@@ -13,7 +13,7 @@ describe Faquiu do
     it "lanzamiento con fuerza automática y resultado negativo" do
         destino = DestinoMock.new false
         faquiu = Faquiu.new destino
-        res = faquiu.lanzar 10
+        res = faquiu.lanzar 1
         expect(res).to eq "Pucha... Lamentablemente esa persona seguirá viviendo un tiempo más :'("
     end
 end
