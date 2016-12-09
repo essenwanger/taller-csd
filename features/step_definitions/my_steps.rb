6bc5600f5c56c25688427f53e104016ce664f3f6
Victor <essenwanger@gmail.com>
2016-12-09 14:57:42 +0000

    steps de ingresar

--- features/step_definitions/my_steps.rb
+++ features/step_definitions/my_steps.rb
@@ -1,1 +1,11 @@
+Given(/^abro el juego$/) do
+  visit '/'
+end
 
+When(/^seleccion "([^"]*)"$/) do |texto|
+  click_button(texto)
+end
+
+Then(/^debo ver "([^"]*)"$/) do |texto|
+  expect(page.body).to match /#{texto}/m
+end
